51ff972ce1eb25e70846746ef4a41b803fc1cd5c
majharul <majharuljony163@gmail.com>
2023-11-17 21:00:50 +0600

    Learn CRUD with relation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { resolvers } from "./resolver";
 import { typeDefs } from "./schema";
+import { resolvers } from "./resolver";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+import { jwtHelper } from "./utils/jwtHelper";
+const prisma = new PrismaClient();
+
+interface Context {
+  prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+  userInfo: {
+    userId: number | null;
+  } | null;
+}
 
 const main = async () => {
   const server = new ApolloServer({
@@ -10,7 +21,14 @@ const main = async () => {
   });
 
   const { url } = await startStandaloneServer(server, {
-    listen: { port: 5000 },
+    listen: { port: 4000 },
+    context: async ({ req }): Promise<Context> => {
+      const userInfo = await jwtHelper.getUserInfoFromToken(req.headers.authorization as string);
+      return {
+        prisma,
+        userInfo,
+      };
+    },
   });
 
   console.log(`🚀  Server ready at: ${url}`);
--- src/schema.ts
+++ src/schema.ts
@@ -1,22 +1,52 @@
 export const typeDefs = `#graphql
     type Query {
-      me: User
-      users: [User]
-      posts: [Post]
+        me: User
+        users: [User]
+        posts: [Post]
+        profile(userId: ID!): Profile
     }
 
     type Mutation {
-      signup(
-          name: String!,
-          email: String!,
-          password: String!
-          bio: String
-      ): AuthPayload,
-      
-      signin(
-          email: String!
-          password: String!
-      ): AuthPayload
+        signup(
+            name: String!,
+            email: String!,
+            password: String!
+            bio: String
+        ): AuthPayload,
+        
+        signin(
+            email: String!
+            password: String!
+        ): AuthPayload,
+
+        addPost(post: PostInput!): PostPayload,
+        updatePost(postId: ID!, post: PostInput!): PostPayload,
+        deletePost(postId: ID!): PostPayload,
+        publishPost(postId: ID!): PostPayload
+    }
+
+    type Post {
+        id: ID!
+        title: String!
+        content: String!
+        author: User
+        createdAt: String!
+        published: Boolean!
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        createdAt: String!
+        posts: [Post]
+    }
+
+    type Profile {
+        id: ID!
+        bio: String!
+        createdAt: String!
+        user: User!
     }
 
     type AuthPayload {
@@ -24,27 +54,13 @@ export const typeDefs = `#graphql
         token: String
     }
 
-    type Post {
-      id: ID!
-      title: String!
-      content: String!
-      author: User
-      createdAt: String!
-      published: Boolean!
+    type PostPayload {
+        userError: String
+        post: Post
     }
 
-    type User {
-      id: ID!
-      name: String!
-      email: String!
-      createdAt: String!
-      posts: [Post]
-    }
-
-    type Profile {
-      id: ID!
-      bio: String!
-      createdAt: String!
-      user: User!
+    input PostInput {
+        title: String
+        content: String
     }
 `;
